eb961d45d54180035a548ed727c2e1d66e69bcd9
Josh Bullock <mail@joshb.me>
2025-09-07 20:06:20 -0400

    More Progress

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -12,9 +12,24 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
+
+# Triangle Rules:
+# * A triangle should not have any sides of length 0 or negative.
+# * Any two sides of a triangle should add up to more than the third side.
+
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides = [a, b, c]
+
+  if sides.any? { |l| l <= 0 }
+    raise TriangleError
+  elsif a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
